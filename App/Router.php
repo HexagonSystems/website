e8ce9e3ce7437c66a7f230308bcd947551d5e1c1
Michael Shane MacDonald <alph4bet@live.com>
2013-09-27 12:01:18 +1000

    Add Routing for packages

--- App/Router.php
+++ App/Router.php
@@ -35,8 +35,9 @@ class Router
             case "navPage":
                 $controller = "NavController";
                 break;
-            case "adminPage":
-                $controller = "AdminController";
+            case "admin":
+                $controller = "AdminRouter";
+                $package = "Admin";
                 break;
             case "viewBlog":
                 $controller = "BlogController";
@@ -52,10 +53,21 @@ class Router
                 break;
         }//end switch
 
+        if(isset($package))
+        {
+        include_once( AppBase.'/Package/'.$package.'/'.$controller.'.php');
+        $router = new $controller();
+        $router->route($conn);
+        }
+        else
+        {
+
         include_once( AppBase.'/Controller/'.$controller.'.php');
         $controller = new $controller();
         $controller->setDatabase($conn);
         $controller->invoke();
+        }
+
     }// end route
     //end class
 }
