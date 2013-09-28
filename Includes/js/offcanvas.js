81b361f3364089b192eedce8f701a5cbb8fee2ad
Stephen McMahon <stephentmcm@gmail.com>
2013-09-28 15:58:06 +1000

    Work on getting bootstrap.html to useable

--- Includes/js/offcanvas.js
+++ Includes/js/offcanvas.js
@@ -2,4 +2,28 @@ $(document).ready(function() {
   $('[data-toggle=offcanvas]').click(function() {
     $('.row-offcanvas').toggleClass('active');
   });
+
+
+    // back to top
+    setTimeout(function () {
+      var $sideBar = $('.sidebar-offcanvas')
+
+      $sideBar.affix({
+        offset: {
+          top: function () {
+            var offsetTop      = $sideBar.offset().top
+            var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
+            var navOuterHeight = $('.navbar-hexagon').height()
+
+            return (this.top = offsetTop - navOuterHeight - sideBarMargin)
+          }
+        , bottom: 0
+        }
+      })
+    }, 100)
+
+    setTimeout(function () {
+      $('.bs-top').affix()
+    }, 100)
+
 });
