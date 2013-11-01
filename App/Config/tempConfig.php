8dc7ac6a7e86f64d3bff4e415f5647a1dc2c5a24
Alex Robinson <alex-robinson@live.com>
2013-11-01 11:22:19 +1100

    ANOTHER MINOR CHANGE Corrected second footer

--- App/Config/tempConfig.php
+++ App/Config/tempConfig.php
@@ -1,180 +1,78 @@
 <?php
-namespace Task;
-class TaskCommentDA
-{
-	private $database = FALSE;
-	
-	public function setDatabase($database)
-	{
-		$this->database = $database;
-	}
-	
-	/**
-	 * Creates a comment in the database
-	 *
-	 * @param int $taskId
-	 * @param int $memberId
-	 * @param string $tag
-	 * @param string $title
-	 * @param string $content
-	 */
-	function createComment($taskId, $memberId, $tag, $title, $content, $time)
-	{
-		try {
-	
-			$statement = 'INSERT INTO `taskComment`
-					(taskId, memberId, title, content, tag, postedDate)
-					VALUES
-					(:taskId, :memberId, :title, :content, :tag, :postedDate)';
-	
-			$query = $this->database->prepare($statement);
-	
-			$query->bindParam(':taskId'   , $taskId , \PDO::PARAM_INT);
-			$query->bindParam(':memberId'   , $memberId , \PDO::PARAM_INT);
-			$query->bindParam(':title'   , $title , \PDO::PARAM_STR);
-			$query->bindParam(':content'   , $content , \PDO::PARAM_STR);
-			$query->bindParam(':tag'   , $tag , \PDO::PARAM_STR);
-			$query->bindParam(':postedDate'   , $time , \PDO::PARAM_INT);
-			/* MIGHT NEED TO GET AUTO INCREMENT COMMENT ID HERE */
-			echo "created comment";
-			$query->execute();
-			return array('success' => true);
-		} catch (PDOException $e) {
-			return createError($e);
-		}
-	}
-	
-	/*
-	 * TEMP FUNCTION, PLEASE FIX AT A LATER DATE
-	 */
-	public function createCommentFromObject($tempTaskComment)
-	{
-		return $this->createComment($tempTaskComment->getTaskId(), $tempTaskComment->getMemberId(), $tempTaskComment->getTag(), $tempTaskComment->getTitle(), $tempTaskComment->getContent(), $tempTaskComment->getDate());
-	}
-	
-	/**
-	 * Loads an existing post from the database
-	 *
-	 * @param int $taskId the id number of the post
-	 * @param int $memberId
-	 * @param int $pageNum
-	 * @param int $qty
-	 *
-	 * @return boolean True for loaded false for DB connection error
-	 */
-	public function loadComments($taskId, $memberId, $pageNum, $qty)
-	{
-		try {
-			$statement = "SELECT comment.*, member.firstName FROM `taskComment` comment 
-					INNER JOIN `member` member ON comment.memberId = member.memberId 
-					WHERE comment.taskId = :taskId 
-					ORDER BY comment.postedDate DESC 
-					LIMIT :starting, :quantity";
-	
-			$query = $this->database->prepare($statement);
-	
-			$query->bindParam(':taskId'   , $taskId , \PDO::PARAM_INT);
-				
-			$starting = ($pageNum - 1) * $qty;
-			
-			$qty = $qty + 0;
-			
-			$query->bindParam(':starting'   , $starting , \PDO::PARAM_INT);
-			$query->bindParam(':quantity'   , $qty		, \PDO::PARAM_INT);
-	
-			if(!$query->execute())
-			{
-				return $this->createError("SQL had trouble executing ");
-			}
-				
-			$commentHolder = array();
-			$commentHolder['success'] = true;
-			$commentHolder['data'] = array();
-			
-				
-			$htmlString = "";
-			foreach ($query as $row) {
-				$tempTaskComment = new TaskComment();
-				$tempTaskComment->setId($row['commentId']);
-				$tempTaskComment->setTag($row['tag']);
-				$tempTaskComment->setTitle($row['title']);
-				$tempTaskComment->setContent($row['content']);
-				$tempTaskComment->setMemberId($row['firstName']);
-				$tempTaskComment->setDate($row['postedDate']);
-	
-				array_push($commentHolder['data'], $tempTaskComment);
-			}
-				
-			// echo json_encode($commentHolder);
-			return $commentHolder;
-		} catch (PDOException $e) {
-			return createError($e);
-		}
-	}//end loadComments
-	
-	/*
-	 * THIS IS A TEMP FUNCTION, PLEASE FIX UP LATER
-	 */
-	function addHoursShort($tempTask)
-	{
-		return $this->addHours($tempTask->getTaskId(), $tempTask->getMemberId(), $tempTask->getDate(), $tempTask->getHours());
-	}
-	
-	/**
-	 * Adds hours for a member into the database
-	 *
-	 * @param int $taskId
-	 * @param int $memberId
-	 * @param date $workedDate
-	 * @param int $workedHours
-	 */
-	function addHours($taskId, $memberId, $workedDate, $workedHours)
-	{
-		try {
-				
-			$statement = 'INSERT INTO `work`
-					(taskId, memberId, hours, date)
-					VALUES
-					(:taskId, :memberId, :hours, :date)
-					ON DUPLICATE KEY UPDATE hours = hours + :hours';
-	
-			$query = DataBase::getConnection()->prepare($statement);
-				
-			$workedDate = date("Y-d-m", strtotime($workedDate));
-	
-			$query->bindParam(':taskId'   , $taskId , \PDO::PARAM_INT);
-			$query->bindParam(':memberId'   , $memberId , \PDO::PARAM_INT);
-			$query->bindParam(':hours'   , $workedHours , \PDO::PARAM_INT);
-			$query->bindParam(':date'   , $workedDate , \PDO::PARAM_STR);
-	
-			if($query->execute())
-			{
-				return array('success' => true);
-			}else
-			{
-				return array('success' => false);
-			}
-	
-				
-		} catch (PDOException $e) {
-			return array('success' => false);
-		}
-	}
-	
-	/**
-	 * Creates an array that holds information about the error
-	 *
-	 * @return string
-	 */
-	public function createError($comment)
-	{
-		$errorMessage = array();
-		$errorMessage['success'] = false;
-		$errorMessage['error']['location'] = "TaskCommentDA";
-		$errorMessage['error']['message'] = $comment;
-		return $errorMessage;
-	}
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * Front controller for config view / download and clear
+ *
+ * @package PhpMyAdmin-Setup
+ */
+
+/**
+ * Core libraries.
+ */
+require './lib/common.inc.php';
+require_once './libraries/config/Form.class.php';
+require_once './libraries/config/FormDisplay.class.php';
+require_once './setup/lib/ConfigGenerator.class.php';
+
+require './libraries/config/setup.forms.php';
+
+$form_display = new FormDisplay();
+$form_display->registerForm('_config.php', $forms['_config.php']);
+$form_display->save('_config.php');
+$config_file_path = ConfigFile::getInstance()->getFilePath();
+
+if (isset($_POST['eol'])) {
+    $_SESSION['eol'] = ($_POST['eol'] == 'unix') ? 'unix' : 'win';
 }
 
-
+if (PMA_ifSetOr($_POST['submit_clear'], '')) {
+    //
+    // Clear current config and return to main page
+    //
+    ConfigFile::getInstance()->resetConfigData();
+    // drop post data
+    header('HTTP/1.1 303 See Other');
+    header('Location: index.php');
+    exit;
+} elseif (PMA_ifSetOr($_POST['submit_download'], '')) {
+    //
+    // Output generated config file
+    //
+    PMA_downloadHeader('config.inc.php', 'text/plain');
+    echo ConfigGenerator::getConfigFile();
+    exit;
+} elseif (PMA_ifSetOr($_POST['submit_save'], '')) {
+    //
+    // Save generated config file on the server
+    //
+    file_put_contents($config_file_path, ConfigGenerator::getConfigFile());
+    header('HTTP/1.1 303 See Other');
+    header('Location: index.php?action_done=config_saved');
+    exit;
+} elseif (PMA_ifSetOr($_POST['submit_load'], '')) {
+    //
+    // Load config file from the server
+    //
+    $cfg = array();
+    include_once $config_file_path;
+    ConfigFile::getInstance()->setConfigData($cfg);
+    header('HTTP/1.1 303 See Other');
+    header('Location: index.php');
+    exit;
+} elseif (PMA_ifSetOr($_POST['submit_delete'], '')) {
+    //
+    // Delete config file on the server
+    //
+    @unlink($config_file_path);
+    header('HTTP/1.1 303 See Other');
+    header('Location: index.php');
+    exit;
+} else {
+    //
+    // Show generated config file in a <textarea>
+    //
+    header('HTTP/1.1 303 See Other');
+    header('Location: index.php?page=config');
+    exit;
+}
 ?>
