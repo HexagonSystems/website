3de3cc0163a2abec94671108a315582de19241e6
tara1624970 <1624970@student.swn.edu.au>
2013-11-18 13:33:13 +1100

    send email function

--- App/Controller/ContactController.php
+++ App/Controller/ContactController.php
@@ -7,22 +7,58 @@ class ContactController extends Controller
     {
         parent::invoke();
         
-        $user = new User($this->database);
-		$resultSet = $user->getAllMembers();
+        $getVars = $_GET;
+		$page = isset($getVars['action']) ? $getVars['action'] : 'empty';
 		
+		$user = new User($this->database);
+		$resultSet = $user->getAllMembers();
 		foreach ($resultSet as $row)
 		{	
-			
 			$members[] = $user->userDetails($row['memberId'], $row['firstName'], $row['lastName'], $row['email'], $row['phoneNo']);
-
 		}
 		
-		$this->template = 'ContactTemplate';
 		
-		$view = new ContactView($this->template,$this->footer);
-		$content =  $members;
-		$view->assign('member', $content);	
-
+		
+		if (isset($_GET['action']))
+		{
+			if ($page == 'email')
+			{
+				if (isset($_POST['msg']))
+				{
+					$sendTo = "1624970@student.swin.edu.au";
+					$subject = "Email via Hexagon Webpage";
+					$content = $_POST['email']."\r\n". $_POST['name']."\r\n".$_POST['msg']."\r\n";
+					$headers = "From: web@hexagon.com.au";
+					//writes email to G:\xampp\mailoutput
+					mail($sendTo, $subject, $content, $headers);
+					
+					$emailSuccessful=true;
+					$this->template = 'ContactTemplate';
+					$view = new View($this->template,$this->footer);
+					$content =  $members;
+					$view->assign('member', $content);
+					$view->assign('note', $emailSuccessful);
+					
+				}
+				else
+				{
+					$emailSuccessful=false;
+					$this->template = 'ContactTemplate';
+					$view = new View($this->template,$this->footer);
+					$content =  $members;
+					$view->assign('member', $content);
+					$view->assign('note', $emailSuccessful);
+				}
+			}
+		}
+		else if (!isset($_get['action'])) 
+		{
+			$this->template = 'ContactTemplate';
+			
+			$view = new View($this->template,$this->footer);
+			$content =  $members;
+			$view->assign('member', $content);	
+		}// end action
     } // end function
 }
 
