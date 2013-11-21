1e20297aaf00c5dc0113b1f266ffb6fc0becdb5c
tara1624970 <1624970@student.swn.edu.au>
2013-11-21 12:33:26 +1100

    added emails to sendto. called function in article bio

--- App/Controller/ContactController.php
+++ App/Controller/ContactController.php
@@ -10,14 +10,15 @@ class ContactController extends Controller
         $getVars = $_GET;
 		$page = isset($getVars['action']) ? $getVars['action'] : 'empty';
 		
-		$user = new User($this->database);
-		$resultSet = $user->getAllMembers();
+		$userObj = new User($this->database);
+		
+		$user = new ArticleBio();
+		$user->setDatabase($this->database);
+		$resultSet = $user->getAllBios();
 		foreach ($resultSet as $row)
 		{	
-			$members[] = $user->userDetails($row['memberId'], $row['firstName'], $row['lastName'], $row['email'], $row['phoneNo']);
+			$members[] = $userObj->userDetails($row['memberId'], $row['firstName'], $row['lastName'], $row['email']);
 		}
-		
-		
 		
 		if (isset($_GET['action']))
 		{
@@ -25,7 +26,7 @@ class ContactController extends Controller
 			{
 				if (isset($_POST['msg']))
 				{
-					$sendTo = "1624970@student.swin.edu.au";
+					$sendTo = "1624970@student.swin.edu.au,". "stephentmcm@gmail.com,"."alex-robinson@live.com,"."9511954@student.swin.edu.au,"."1732862@student.swin.edu.au";
 					$subject = "Email via Hexagon Webpage";
 					$content = $_POST['email']."\r\n". $_POST['name']."\r\n".$_POST['msg']."\r\n";
 					$headers = "From: web@hexagon.com.au";
