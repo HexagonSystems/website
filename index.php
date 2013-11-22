56ede9e3a882266af8ecedc468dcb01171feaad2
Alex Robinson <alex-robinson@live.com>
2013-11-22 11:20:12 +1100

    Testing a reload function if /website/ isn't present in the URL address on mercury

--- index.php
+++ index.php
@@ -7,6 +7,10 @@ session_start();
 require_once('App/Config/Config.php');
 require_once("App/Router.php");
 
+if (strpos($_SERVER['REQUEST_URI'],'index.php') === false) {
+	header('Location: '.SITE_ROOT.'index.php');
+}
+
 Router::route(DataBase::getConnection());
 
 ?>
