c3ad6cfe3f72db62331268d4e5f2fbaf0ad57146
tara1624970 <1624970@student.swn.edu.au>
2013-12-11 15:21:44 +1100

    created files to work with pop up help on admin page

---
+++ App/Package/Admin/View/Template/helpAdminEdit.php
@@ -0,0 +1,35 @@
+<strong>Edit an Article</strong>
+<p>
+	To edit an article, fill in the appropriate fields and press save. You will be then taken back to the main admin page.<br /> If you wish to leave the page without saving simply navigate way from the page.
+</p>
+<p>An acceptable edit includes 6 fields:</p>
+<ul>
+	<li><strong>Title - </strong>This is the title of the Article. If the code is 1 the title will be a name of the code is 2 it will be the project title.<br/> This will not accept numbers.
+	</li>
+	<li>
+		<strong>Content - </strong>Here you should give a description. If you are editing a bio please enter an image path.
+	</li>
+	<li>
+		<strong>Category Code - </strong>This field allocates where an article will be displayed. 
+		<ul>
+			<li>1 = Bio. This will be displayed in the About section and an image path should be written in the content section</li>
+			<li>2 = Project. This will be displayed in the Projects section and a corresponding page will be setup.</li>
+			<li>3 = File. A file must be uploaded for this code to function correctly</li>
+		</ul>
+		This field only takes a number.
+	</li>
+	<li>
+		<strong>Status - </strong>This is only in use for editing a project. If the article is of a different code it may be left blank.<br/>
+	</li>
+	<li>
+		<strong>Tag - </strong>A tag represents a type of article or language type. <br/> 
+		If you are editing a project (code 2), the information entered is output under the heading Language.
+	</li>
+	<li>
+		<strong>Date - </strong>The date entered will be the date you wish each article completed by. for example if you add 1999-05-02 the date completed will be May 2009. <br/>
+		For the form to accept a date, the format must be year-month-day (yyyy-mm-dd).
+	</li>
+</ul>
+
+<p>Once you have filled out all of these details, click 'Save' and you will be able to view the updated content on the admin page.</p>
+<br />
